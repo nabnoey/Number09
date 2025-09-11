267b5a5dbf171ce648d124b38d83fa8b7c19da5d
nabnoey <nabnoeyy@gmail.com>
2025-09-11 15:51:10 +0700

    Update UDSTokenEx.sol
    update2

--- contracts/UDSTokenEx.sol
+++ contracts/UDSTokenEx.sol
@@ -1,35 +1,36 @@
 // SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.4.0
 pragma solidity ^0.8.30;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract UDSTokenEx is ERC20 {
-    uint256 public unitsOneTokenToBuy = 10;
+    uint256 uintsOneTokenToBuy = 10;
+    event Buy(address indexed form, address indexed to, uint256 tokens);
     address public owner;
-     
-    event Buy(address indexed from,address indexed to,uint token);
+    mapping(address => uint256) balances;
 
- constructor() ERC20("UdsaneeToken", "UDS") {
-        owner = msg.sender;
+    constructor() ERC20("UDSTokenEx", "UDS") {
         _mint(address(this), 1000000 * 10 ** decimals());
+        owner = msg.sender;
     }
 
-  modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner can call this function");
+    modifier onlyOwner() {
+        require(msg.sender == owner);
         _;
-    }          
+    }
 
-function buy() payable public  {
-      require(msg.value > 0, "There is no ETH to exchange");// เช็คว่ามี ETH ที่จะมาแลกมั้ย
-        uint256 amount = msg.value * unitsOneTokenToBuy;//ใส่ ETH เข้ามาเท่าไร
-        require(amount > 0, "Too less amount of token");// เช็คว่ามี Token มากกว่า 0 คือมี UDS Token ให้แลกนะ
-        require(balanceOf(address(this)) >= amount, "There is no token left. Sold out!!");
-         _transfer(address(this), msg.sender, amount);
+    function buy() payable public {
+        balances[msg.sender] += msg.value;
+        uint256 amount = balances[msg.sender] * uintsOneTokenToBuy;
+        require(amount > 0, "Too less amount of token");
+        require(balanceOf(address(this)) >= amount, "There is no token left. Sold out!");
+        _transfer(address(this), msg.sender, amount);
         emit Buy(address(this), msg.sender, amount);
     }
 
-// owner คนที่ deploy contract สามารถ withdraw ETH ได้
-  function withdraw(uint256 amt) public onlyOwner {
+     function withdraw(uint256 amt) public onlyOwner {
         require(address(this).balance >= amt, "Not enough ETH in contract");
         payable(owner).transfer(amt);
     }
